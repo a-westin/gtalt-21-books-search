729a01f92ca6f9f3ae365e52ddb139d7cf58dafa
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-11-09 23:26:51 -0500

    created navbar and footer components

---
+++ books-search/src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-1">
+      <div className="container-fluid">
+        <div className="row">
+          <span className="col-sm-4 text-center my-auto">
+            Copyright&copy; 2020 AFW
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
