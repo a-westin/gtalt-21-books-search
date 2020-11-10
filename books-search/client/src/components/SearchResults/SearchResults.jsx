087ffb57ee2a54e7e3e50949d8d1b58b6ad72683
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-11-10 18:33:51 -0500

    created SearchResults.jsx

---
+++ books-search/client/src/components/SearchResults/SearchResults.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const SearchResults = ({ book, saveBook }) => {
+  return (
+    <div className="col-sm-12">
+      <div className="card mb-3">
+        <div className="row">
+          <div className="col-md-2 text-center">
+            <img
+              src={book.imageLinks ? book.imageLinks.thumbnail : "https://upload.wikimedia.org/wikipedia/commons/7/72/Placeholder_book.svg"}
+              className="card-img p-3 search-image text-center"
+              alt={book.title ? book.title : "Untiitled"}
+            />
+          </div>
+          <div className="col-md-10">
+            <div className="card-body text-center">
+              <h5 className="card-title">{book.title ? book.title : "No Title"}</h5>
+              <h6 className="card-text">
+                Author: {book.authors ? book.authors.join(", ") : []}
+              </h6>
+              <p className="card-text">{book.description ? book.description : ""}</p>
+              <a
+                href={book.infoLink ? book.infoLink : "https://google.com"}
+                className="btn btn-info mr-1"
+                role="button"
+                target="blank"
+              >
+                Description
+              </a>
+              <button
+                className="btn btn-info ml-1"
+                onClick={(e) => saveBook(e, book)}
+              >
+                Save 
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchResults;
