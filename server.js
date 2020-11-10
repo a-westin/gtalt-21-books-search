a5c44c12690c6b03ddfba8af4d1c42e0951423df
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-11-09 23:02:25 -0500

    building server routes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("client/build"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/books_search", {
   useNewUrlParser: true,
@@ -27,6 +28,10 @@ connection.on("error", (err) => {
   console.log("Connection error: ", err);
 });
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+  
 app.get("/api/config", (req, res) => {
   res.json({
     success: true,
