68135b709f8f3df13a28501b69836973cbb23178
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-11-10 18:53:29 -0500

    created NoBooksCard.jsx

---
+++ books-search/client/src/components/NoBooksCard/NoBooksCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NoBooksCard = (props) => {
+  return (
+    <div className="col-sm-12 text-center">
+      <div className={props.className}>
+        <div className="col-md-12">
+          <div className="card-body text-center">
+            <h5 className="card-title">{props.message}</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoBooksCard;
