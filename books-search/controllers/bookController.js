e4871947be72d38a485ace846940a5bd86d24322
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-11-10 19:33:59 -0500

    set up bookController.js

---
+++ books-search/controllers/bookController.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../models");
+
+router.get("/", (req, res) => {
+  db.Book.find({})
+    .then((foundBooks) => {
+      res.json(foundBooks);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: true,
+        data: null,
+        message: "Couldn't retrieve books",
+      });
+    });
+});
+
+router.post("/", (req, res) => {
+  const newBook = {
+    title: req.body.title,
+    authors: req.body.authors,
+    description: req.body.description,
+    image: req.body.image,
+    link: req.body.link,
+  };
+  db.Book.create(newBook)
+    .then((newBook) => res.json(newBook))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: true,
+        data: null,
+        message: "Couldn't add book",
+      });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  db.Book.findByIdAndDelete(req.params.id)
+    .then((deletedBook) => {
+      res.json(deletedBook);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: true,
+        data: null,
+        message: "Couldn't delete book",
+      });
+    });
+});
+
+module.exports = router;
